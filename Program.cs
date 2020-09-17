3b0d41bb5214ea3ac262d13509ccedda7db2e403
Sara Martinez <s_martinez_1990@outlook.com>
2020-09-16 22:28:04 -0400

    code completed

--- Program.cs
+++ Program.cs
@@ -6,7 +6,13 @@ namespace EvenNums
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Even numbers from 0 - 100 are: ");
+
+            var number = 0;
+            while (number < 100) {
+                number = number + 2;
+                Console.WriteLine(number);                
+            }
         }
     }
 }
